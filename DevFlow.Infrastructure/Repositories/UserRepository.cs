2c9dd2984717d7fca59ecfdec9b87f7095cfe4bf
Muhammad Bilal <155463851+MuhammadBilal64@users.noreply.github.com>
2026-06-12 23:07:08 +0500

    feat(infrastructure): add UserRepository

---
+++ DevFlow.Infrastructure/Repositories/UserRepository.cs
@@ -0,0 +1,28 @@
+﻿using DevFlow.Application.Users;
+using DevFlow.Domain.Entities;
+using DevFlow.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+
+namespace DevFlow.Infrastructure.Repositories
+{
+    public  class UserRepository:IUserRepository
+    {
+        private readonly DevFlowDbContext _context;
+
+        public UserRepository(DevFlowDbContext context)
+        {
+            _context= context;
+        }
+        public async Task<bool> ExistByEmailAsync(string email)
+        {
+            return await _context.Users.AnyAsync(u => u.Email == email);
+
+        }
+        public async Task AddAsync(User user)
+        {
+            await _context.Users.AddAsync(user);
+            await _context.SaveChangesAsync();
+        }
+
+    }
+}
--- DevFlow/Program.cs
+++ DevFlow/Program.cs
@@ -1,4 +1,6 @@
+using DevFlow.Application.Users.RegisterUser;
 using DevFlow.Infrastructure.Persistence;
+using DevFlow.Infrastructure.Repositories;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -8,6 +10,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddScoped<IUserRepository, UserRepository>();
 builder.Services.AddDbContext<DevFlowDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DevFlowDb")));
 var app = builder.Build();
