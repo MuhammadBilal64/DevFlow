33022cb3466b0818aff4c2da3f0b4bb3e91148f3
Muhammad Bilal <155463851+MuhammadBilal64@users.noreply.github.com>
2026-06-13 13:54:55 +0500

    feat(auth): add login handler with BCrypt password verification

--- DevFlow.Application/Users/LoginUser/LoginUserHandle.cs
+++ DevFlow.Application/Users/LoginUser/LoginUserHandle.cs
@@ -1,11 +1,40 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using DevFlow.Application.Abstractions;
 
 namespace DevFlow.Application.Users.LoginUser
 {
     public class LoginUserHandle
     {
+        private readonly IUserRepository _userRepository;
+        private readonly IPasswordHasher _passwordHasher;
+        public LoginUserHandle(IUserRepository userRepository, IPasswordHasher passwordHasher)
+        {
+            _userRepository = userRepository;
+            _passwordHasher = passwordHasher;
+        }
+        public async Task<LoginUserResult> Handle(LoginUserCommand command)
+        {
+            var user =  await _userRepository.GetByEmailAsync(command.Email);
+            if (user == null)
+            {
+                throw new Exception("User not Registered");
+            }
+           var hashed = _passwordHasher.Hash(command.Password);
+            bool verify = _passwordHasher.verify(user., hashed);
+            if (verify == false)
+            {
+                throw new Exception("Password Not Matched");
+            }
+            return new LoginUserResult
+            {
+                UserId = user.Id,
+                Email = user.Email,
+                Role = user.Role,
+                Name = user.Name,
+            };
 
+        }
     }
 }
--- DevFlow.Application/Users/LoginUser/LoginUserResult.cs
+++ DevFlow.Application/Users/LoginUser/LoginUserResult.cs
@@ -1,10 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using DevFlow.Domain.Enum;
 
 namespace DevFlow.Application.Users.LoginUser
 {
     public class LoginUserResult
     {
+        public int UserId {  get; set; }
+        public string Name { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public UserRole Role { get; set; }
     }
 }
