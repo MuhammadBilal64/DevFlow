582eb8db107bcce2d72322ddd37b373d371c7fa7
Muhammad Bilal <155463851+MuhammadBilal64@users.noreply.github.com>
2026-06-26 00:46:52 +0500

    feat:add TaskItem entity and migrations

---
+++ DevFlow.Domain/Enum/TaskPriority.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DevFlow.Domain.Enum
+{
+    public enum TaskPriority
+    {
+        Low,
+        Medium,
+        High,
+    }
+}
---
+++ DevFlow.Infrastructure/Persistence/Configurations/TaskItemConfiguration.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DevFlow.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace DevFlow.Infrastructure.Persistence.Configurations
+{
+    public class TaskItemConfiguration : IEntityTypeConfiguration<TaskItem>
+    {
+        public void Configure(EntityTypeBuilder<TaskItem> builder)
+        {
+            builder
+                .HasOne(p => p.Project)
+                .WithMany(t => t.Tasks)
+                .HasForeignKey(i => i.ProjectId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder
+                .HasOne(i => i.Creator)
+                .WithMany(y => y.CreatedTasks)
+                .HasForeignKey(p => p.CreatedBy)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder
+                .HasOne(p => p.Assignee)
+                .WithMany(p => p.AssignedTasks)
+                .HasForeignKey(s => s.AssignedToUserId)
+                .OnDelete(DeleteBehavior.Restrict);
+        }
+    }
+}
