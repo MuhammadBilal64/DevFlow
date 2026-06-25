4a583f2570171b5c9629e2974e4f84c742d58832
Muhammad Bilal <155463851+MuhammadBilal64@users.noreply.github.com>
2026-06-26 00:48:25 +0500

    feat: add CreateTask command, handler and repository

---
+++ DevFlow.Application/Tasks/CreateTask/CreateTaskCommand.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DevFlow.Domain.Enum;
+using MediatR;
+
+namespace DevFlow.Application.Tasks.CreateTask
+{
+    public class CreateTaskCommand:IRequest<CreateTaskResult>
+    {
+        public int ProjectId {  get; set; }
+        public string Title { get; set; } = null!;
+        public string Description { get; set; } = null!;
+        public TaskPriority Priority { get; set; }
+        public DateTime? DueDate { get; set; }
+        public int? AssignedToUserId { get; set; }
+    }
+}
---
+++ DevFlow.Application/Tasks/CreateTask/CreateTaskHandler.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DevFlow.Application.Abstractions;
+using DevFlow.Application.Common.Interfaces;
+using DevFlow.Application.Exceptions;
+using DevFlow.Domain.Entities;
+using MediatR;
+
+namespace DevFlow.Application.Tasks.CreateTask
+{
+    public class CreateTaskHandler : IRequestHandler<CreateTaskCommand, CreateTaskResult>
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IWorkspaceMemberRepository _workspaceMemberRepository;
+        private readonly ITaskRepository _taskRepository;
+        private readonly IProjectRepository _projectRepository;
+        private readonly ICurrentUserService _currentUserService;
+        public CreateTaskHandler(ICurrentUserService currentUserService,IProjectRepository projectRepository,IUnitOfWork unitOfWork, IWorkspaceMemberRepository workspaceMemberRepository, ITaskRepository taskRepository)
+        {
+            _unitOfWork = unitOfWork;
+            _workspaceMemberRepository = workspaceMemberRepository;
+            _taskRepository = taskRepository;
+            _projectRepository = projectRepository;
+            _currentUserService= currentUserService;
+        }
+
+        public async Task<CreateTaskResult> Handle(CreateTaskCommand request, CancellationToken cancellationToken)
+        {
+            var project =await  _projectRepository.GetByIdAsync(request.ProjectId);
+            if (project == null)
+            {
+                throw new NotFoundException("Project Doesnot Exist");
+            }
+            var userId =   _currentUserService.UserId;
+            var membership = await _workspaceMemberRepository.GetMemberAsync(userId, project.WorkspaceId);
+            if (membership == null)
+            {
+                throw new ForbiddenException("You are not allowed to Add");
+            }
+            if (request.AssignedToUserId != null)
+            {
+                var member = await _workspaceMemberRepository.GetMemberAsync((int)request.AssignedToUserId, project.WorkspaceId);
+                if (member == null)
+                {
+                    throw new ForbiddenException(
+                        "User is not a member of the workspace");
+                }
+               
+
+
+
+            }
+
+            var task= new TaskItem
+            {
+                Title = request.Title,
+                Description = request.Description,
+                ProjectId = project.Id,
+
+                CreatedBy = userId,
+                CreatedAt = DateTime.UtcNow,
+
+                AssignedToUserId = request.AssignedToUserId,
+
+                AssignedAt = request.AssignedToUserId != null
+    ? DateTime.UtcNow
+    : null,
+                DueDate = request.DueDate,
+
+                Priority = request.Priority,
+
+                Status = Domain.Enum.TaskStatus.Todo,
+       
+            };
+            await _taskRepository.AddAsync(task);
+            await _unitOfWork.SaveChangesAsync();
+            var result = new CreateTaskResult
+            {
+                TaskId=task.Id,
+                Title= task.Title,
+                Description= task.Description,
+            };
+            return result;
+        }
+    }
+}
