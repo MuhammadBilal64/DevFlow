d06cc9433e8255894dd82bb75b8c053786c07e0e
Muhammad Bilal <155463851+MuhammadBilal64@users.noreply.github.com>
2026-07-02 14:38:29 +0500

    feat:add TaskAssigned handler; MediatR domain events

--- DevFlow.Domain/Events/IDomainEvent.cs
+++ DevFlow.Domain/Events/IDomainEvent.cs
@@ -1,10 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
-
+using MediatR;
 namespace DevFlow.Domain.Events
 {
-    public interface IDomainEvent
+    public interface IDomainEvent:INotification
     {
 
     }
