5e0dd7b49ea104fa0728a832aea916f9b70673f6
Muhammad Bilal <155463851+MuhammadBilal64@users.noreply.github.com>
2026-07-06 14:47:12 +0500

    feat:add WorkflowRepository implementation

---
+++ DevFlow.Application/Abstractions/IWorkflowRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DevFlow.Domain.Entities;
+using DevFlow.Domain.Enum;
+
+namespace DevFlow.Application.Abstractions
+{
+    public interface IWorkflowRepository
+    {
+        Task AddAsync(Workflow workflow);
+
+        Task<Workflow?> GetByIdAsync(int workflowId);
+
+        Task UpdateAsync(Workflow workflow);
+
+        Task DeleteAsync(Workflow workflow);
+
+        Task<IReadOnlyList<Workflow>> GetActiveByTriggerAsync(
+            WorkflowTrigger trigger);
+    }
+}
---
+++ DevFlow.Infrastructure/Repositories/WorkflowRepository.cs
@@ -0,0 +1,53 @@
+﻿using DevFlow.Application.Abstractions;
+using DevFlow.Domain.Entities;
+using DevFlow.Domain.Enum;
+using DevFlow.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+
+namespace DevFlow.Infrastructure.Repositories
+{
+    public class WorkflowRepository : IWorkflowRepository
+    {
+        private readonly DevFlowDbContext _context;
+
+        public WorkflowRepository(DevFlowDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task AddAsync(Workflow workflow)
+        {
+            await _context.Workflows.AddAsync(workflow);
+        }
+
+        public Task UpdateAsync(Workflow workflow)
+        {
+            _context.Workflows.Update(workflow);
+            return Task.CompletedTask;
+        }
+
+        public Task DeleteAsync(Workflow workflow)
+        {
+            _context.Workflows.Remove(workflow);
+            return Task.CompletedTask;
+        }
+
+        public async Task<Workflow?> GetByIdAsync(int workflowId)
+        {
+            return await _context.Workflows
+                .Include(w => w.Conditions)
+                .Include(w => w.Actions)
+                .FirstOrDefaultAsync(w => w.Id == workflowId);
+        }
+
+        public async Task<IReadOnlyList<Workflow>> GetActiveByTriggerAsync(
+            WorkflowTrigger trigger)
+        {
+            return await _context.Workflows
+                .Include(w => w.Conditions)
+                .Include(w => w.Actions)
+                .Where(w => w.Trigger == trigger && w.IsEnabled)
+                .ToListAsync();
+        }
+    }
+}
