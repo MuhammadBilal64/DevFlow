c70ad97819b797594647d394cc8a1475147f9a40
Muhammad Bilal <155463851+MuhammadBilal64@users.noreply.github.com>
2026-07-09 22:08:14 +0500

    feat:add GetWorkflowById query handler

---
+++ DevFlow.Application/Workflows/GetWorkflowById/GetWorkflowByIdHandler.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DevFlow.Application.Abstractions;
+using DevFlow.Application.Exceptions;
+using DevFlow.Application.Workflows.WorkflowDtos;
+using MediatR;
+
+namespace DevFlow.Application.Workflows.GetWorkflowById
+{
+    public class GetWorkflowByIdHandler
+    : IRequestHandler<GetWorkflowByIdQuery, GetWorkflowByIdResult>
+    {
+        private readonly IWorkflowRepository _workflowRepository;
+
+        public GetWorkflowByIdHandler(
+            IWorkflowRepository workflowRepository)
+        {
+            _workflowRepository = workflowRepository;
+        }
+
+        public async Task<GetWorkflowByIdResult> Handle(
+            GetWorkflowByIdQuery request,
+            CancellationToken cancellationToken)
+        {
+            var workflow =
+                await _workflowRepository.GetByIdAsync(request.WorkflowId);
+
+            if (workflow == null)
+            {
+                throw new NotFoundException("Workflow does not exist.");
+            }
+
+            return new GetWorkflowByIdResult
+            {
+                Id = workflow.Id,
+                Name = workflow.Name,
+                Description = workflow.Description,
+                Trigger = workflow.Trigger,
+                IsEnabled = workflow.IsEnabled,
+
+                Conditions = workflow.Conditions
+                    .Select(c => new WorkflowConditionDto
+                    {
+                        Field = c.Field,
+                        Operator = c.Operator,
+                        Value = c.Value
+                    })
+                    .ToList(),
+
+                Actions = workflow.Actions
+                   .OrderBy(a => a.Order)
+                    .Select(a => new WorkflowActionDto
+                    {
+                        ActionType = a.ActionType,
+                        Parameters = a.Parameters,
+                        Order = a.Order
+                    })
+                    .ToList()
+            };
+        }
+    }
+}
---
+++ DevFlow.Application/Workflows/GetWorkflowById/GetWorkflowByIdQuery.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MediatR;
+
+namespace DevFlow.Application.Workflows.GetWorkflowById
+{
+    public class GetWorkflowByIdQuery:IRequest<GetWorkflowByIdResult>
+    {
+        public int WorkflowId {  get; set; }
+    }
+}
