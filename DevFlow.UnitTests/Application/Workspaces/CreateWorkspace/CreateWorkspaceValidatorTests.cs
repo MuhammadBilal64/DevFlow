84288f066b855b51f431610903043d7b370d304c
Muhammad Bilal <155463851+MuhammadBilal64@users.noreply.github.com>
2026-07-24 16:20:08 +0500

    feat:add unit tests for CreateWorkspace validator

---
+++ DevFlow.UnitTests/Application/Workspaces/CreateWorkspace/CreateWorkspaceValidatorTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DevFlow.Application.Workspaces.CreateWorkspace;
+using FluentValidation.TestHelper;
+using Xunit;
+
+namespace DevFlow.UnitTests.Application.Workspaces
+{
+    public class CreateWorkspaceValidatorTests
+    {
+        [Fact]
+        public void Should_Have_Error_When_Name_Is_Empty()
+        {
+            //arrange
+            var validator = new CreateWorkspaceValidator();
+            var command = new CreateWorkspaceCommand
+            {
+                Name = string.Empty
+            };
+            //act
+            var result = validator.TestValidate(command);
+            //assert
+            result.ShouldHaveValidationErrorFor(x => x.Name);
+
+
+
+
+        }
+
+
+    }
+}
