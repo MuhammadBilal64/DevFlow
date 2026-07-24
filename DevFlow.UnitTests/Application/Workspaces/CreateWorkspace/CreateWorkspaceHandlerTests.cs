97bb212be707a70062a6f8e683fa0fcd7b9577a3
Muhammad Bilal <155463851+MuhammadBilal64@users.noreply.github.com>
2026-07-24 23:30:29 +0500

    feat:add workspace handler & validator unit tests

---
+++ DevFlow.UnitTests/Application/Workspaces/CreateWorkspace/CreateWorkspaceHandlerTests.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DevFlow.Application.Abstractions;
+using DevFlow.Application.Common.Interfaces;
+using DevFlow.Application.Workspaces.CreateWorkspace;
+using DevFlow.Domain.Entities;
+using DevFlow.Domain.Enum;
+using FluentAssertions;
+using Moq;
+using Xunit;
+
+namespace DevFlow.UnitTests.Application.Workspaces.CreateWorkspace
+{
+    public class CreateWorkspaceHandlerTests
+    {
+        [Fact]
+        public async Task Should_Create_Workspace_Successfully()
+        {
+           
+            // Arrange
+            var workspaceRepositoryMock = new Mock<IWorkspaceRepository>();
+
+            var workspaceMemberRepositoryMock = new Mock<IWorkspaceMemberRepository>();
+
+            var currentUserServiceMock = new Mock<ICurrentUserService>();
+
+            var unitOfWorkMock = new Mock<IUnitOfWork>();
+            currentUserServiceMock.Setup(x => x.UserId).Returns(1);
+            var handler = new CreateWorkspaceHandler(
+            unitOfWorkMock.Object,
+         workspaceRepositoryMock.Object,
+         currentUserServiceMock.Object,
+         workspaceMemberRepositoryMock.Object);
+            var command = new CreateWorkspaceCommand
+            {
+                Name = "Development"
+            };
+            //Act
+            var result = await handler.Handle(command, CancellationToken.None);
+            //assert
+            result.Should().NotBeNull();
+            result.Name.Should().Be("Development");
+            // Verify interactions
+
+            workspaceRepositoryMock.Verify(
+     x => x.AddAsync(It.Is<Workspace>(w =>
+         w.Name == "Development" &&
+         w.CreatedBy == 1)),
+     Times.Once);
+
+            workspaceMemberRepositoryMock.Verify(
+    x => x.AddAsync(It.Is<WorkspaceMember>(m =>
+        m.UserId == 1 &&
+        m.Role == WorkspaceRole.Owner)),
+    Times.Once);
+
+            unitOfWorkMock.Verify(
+    x => x.SaveChangesAsync(),
+    Times.Once);
+
+
+
+
+        }
+    }
+}
---
+++ DevFlow.UnitTests/Application/Workspaces/GetWorkspaceById/GetWorkspaceByIdHandlerTests.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DevFlow.Application.Abstractions;
+using DevFlow.Application.Common.Interfaces;
+using DevFlow.Application.Workspaces.GetWorkspaceById;
+using DevFlow.Domain.Entities;
+using FluentAssertions;
+using Moq;
+using Xunit;
+
+namespace DevFlow.UnitTests.Application.Workspaces.GetWorkspaceById
+{
+    public class GetWorkspaceByIdHandlerTests
+    {
+        [Fact]
+        public async Task Should_Return_Workspace_When_User_Is_A_Member()
+        {
+            // Arrange
+
+            var workspaceRepositoryMock = new Mock<IWorkspaceRepository>();
+
+            var workspaceMemberRepositoryMock = new Mock<IWorkspaceMemberRepository>();
+
+            var currentUserServiceMock = new Mock<ICurrentUserService>();
+            currentUserServiceMock
+    .Setup(x => x.UserId)
+    .Returns(1);
+            var member = new WorkspaceMember
+            {
+                UserId = 1,
+                WorkspaceId = 10
+            };
+
+            var workspace = new Workspace
+            {
+                Id = 10,
+                Name = "Development",
+                CreatedAt = DateTime.UtcNow
+            };
+            workspaceMemberRepositoryMock
+    .Setup(x => x.GetMemberAsync(1, 10))
+    .ReturnsAsync(member);
+
+            workspaceRepositoryMock
+                .Setup(x => x.GetByIdAsync(10))
+                .ReturnsAsync(workspace);
+
+            var handler = new GetWorkspaceByIdHandler(
+    currentUserServiceMock.Object,
+    workspaceMemberRepositoryMock.Object,
+    workspaceRepositoryMock.Object);
+            var query = new GetWorkspaceByIdQuery
+            {
+                WorkspaceId = 10
+            };
+            // Act
+            var result = await handler.Handle(query, CancellationToken.None);
+            // Assert
+            result.Should().NotBeNull();
+            result.Id.Should().Be(10);
+            result.Name.Should().Be("Development");
+            result.CreatedAt.Should().Be(workspace.CreatedAt);
+            // Verify
+            workspaceMemberRepositoryMock.Verify(
+                x => x.GetMemberAsync(1, 10),
+                Times.Once);
+
+            workspaceRepositoryMock.Verify(
+                x => x.GetByIdAsync(10),
+                Times.Once);
+
+        }
+    }
+}
