00ad36c3604ee134ccad7824952804ca64fe7d87
Muhammad Bilal <155463851+MuhammadBilal64@users.noreply.github.com>
2026-07-27 15:12:33 +0500

    feat:add unit tests for UpdateTaskStatus feature

---
+++ DevFlow.UnitTests/Application/Tasks/UpdateTaskStatus/UpdateTaskStatusValidatorTests.cs
@@ -0,0 +1,67 @@
+﻿using DevFlow.Application.Tasks.UpdateTaskStatus;
+using FluentValidation.TestHelper;
+using Xunit;
+using TaskStatusEnum = DevFlow.Domain.Enum.TaskStatus;
+
+namespace DevFlow.UnitTests.Application.Tasks.UpdateTaskStatus
+{
+    public class UpdateTaskStatusValidatorTests
+    {
+        private readonly UpdateTaskStatusValidator _validator = new();
+
+        [Fact]
+        public void Should_Not_Have_Error_When_Command_Is_Valid()
+        {
+            // Arrange
+            var command = new UpdateTaskStatusCommand
+            {
+                TaskId = 1,
+                TaskStatus = TaskStatusEnum.InProgress
+            };
+
+            // Act
+            var result = _validator.TestValidate(command);
+
+            // Assert
+            result.ShouldNotHaveAnyValidationErrors();
+        }
+
+        [Theory]
+        [InlineData(0)]
+        [InlineData(-1)]
+        public void Should_Have_Error_When_TaskId_Is_Invalid(int taskId)
+        {
+            // Arrange
+            var command = new UpdateTaskStatusCommand
+            {
+                TaskId = taskId,
+                TaskStatus = TaskStatusEnum.Todo
+            };
+
+            // Act
+            var result = _validator.TestValidate(command);
+
+            // Assert
+            result.ShouldHaveValidationErrorFor(x => x.TaskId);
+        }
+
+        [Theory]
+        [InlineData((TaskStatusEnum)100)]
+        [InlineData((TaskStatusEnum)(-1))]
+        public void Should_Have_Error_When_TaskStatus_Is_Invalid(TaskStatusEnum status)
+        {
+            // Arrange
+            var command = new UpdateTaskStatusCommand
+            {
+                TaskId = 1,
+                TaskStatus = status
+            };
+
+            // Act
+            var result = _validator.TestValidate(command);
+
+            // Assert
+            result.ShouldHaveValidationErrorFor(x => x.TaskStatus);
+        }
+    }
+}
