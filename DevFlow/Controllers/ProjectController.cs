81dfabd0fac2703c85777566272e266e6452f813
Muhammad Bilal <155463851+MuhammadBilal64@users.noreply.github.com>
2026-07-31 17:29:48 +0500

    feat:add project members endpoints to ProjectController

--- DevFlow/Controllers/ProjectController.cs
+++ DevFlow/Controllers/ProjectController.cs
@@ -1,5 +1,8 @@
 ﻿using DevFlow.Api.Contracts.Responses;
 using DevFlow.Application.Common.Models;
+using DevFlow.Application.ProjectMembers.AddProjectMember;
+using DevFlow.Application.ProjectMembers.GetProjectMembers;
+using DevFlow.Application.ProjectMembers.RemoveProjectMember;
 using DevFlow.Application.Projects.CreateProject;
 using DevFlow.Application.Projects.GetMyProjectsByWorkspace;
 using DevFlow.Application.Projects.GetProjectById;
@@ -52,6 +55,54 @@ namespace DevFlow.Api.Controllers
             command.ProjectId = ProjectId;
             var result = await _mediator.Send(command);
             return Ok(ApiResponse<UpdateProjectResult>.Ok(result, "Updated Successfully"));
+        }
+        [HttpPost("{projectId}/members")]
+        [Authorize]
+        public async Task<IActionResult> AddProjectMember(
+    [FromRoute] int projectId,
+    AddProjectMemberCommand command)
+        {
+            command.ProjectId = projectId;
+
+            var result = await _mediator.Send(command);
+
+            return Ok(ApiResponse<AddProjectMemberResult>.Ok(
+                result,
+                "Project member added successfully"));
+        }
+
+        [HttpGet("{projectId}/members")]
+        [Authorize]
+        public async Task<IActionResult> GetProjectMembers(
+            [FromRoute] int projectId)
+        {
+            var result = await _mediator.Send(new GetProjectMembersQuery
+            {
+                ProjectId = projectId
+            });
+
+            return Ok(ApiResponse<List<GetProjectMembersResult>>.Ok(
+                result,
+                "Retrieved successfully"));
+        }
+
+        [HttpDelete("{projectId}/members/{userId}")]
+        [Authorize]
+        public async Task<IActionResult> RemoveProjectMember(
+            [FromRoute] int projectId,
+            [FromRoute] int userId)
+        {
+            var command = new RemoveProjectMemberCommand
+            {
+                ProjectId = projectId,
+                UserId = userId
+            };
+
+            var result = await _mediator.Send(command);
+
+            return Ok(ApiResponse<RemoveProjectMemberResult>.Ok(
+                result,
+                "Project member removed successfully"));
         }
 
     }
