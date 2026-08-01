33eb49fcc642e6332f26accb3afe9238f44ffe59
Muhammad Bilal <155463851+MuhammadBilal64@users.noreply.github.com>
2026-08-01 12:01:32 +0500

    Update Program.cs

--- DevFlow/Program.cs
+++ DevFlow/Program.cs
@@ -28,9 +28,10 @@ builder.Services.AddCors(options =>
     options.AddPolicy("DevFlowFrontend", policy =>
     {
         policy
-            .WithOrigins("http://localhost:5173")
+            .WithOrigins("http://localhost:5173", "http://localhost:5174", "http://localhost:3000")
             .AllowAnyHeader()
-            .AllowAnyMethod();
+            .AllowAnyMethod()
+            .AllowCredentials();
     });
 });
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
@@ -99,12 +100,23 @@ builder.Services.AddAuthentication(options =>
         ValidIssuer = jwtsettings!.Issuer,
         ValidAudience = jwtsettings.Audience,
         IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtsettings.Key))
-
-
-
     };
 
+    options.Events = new JwtBearerEvents
+    {
+        OnMessageReceived = context =>
+        {
+            var accessToken = context.Request.Query["access_token"];
+            var path = context.HttpContext.Request.Path;
+            if (!string.IsNullOrEmpty(accessToken) && path.StartsWithSegments("/notificationHub"))
+            {
+                context.Token = accessToken;
+            }
+            return Task.CompletedTask;
+        }
+    };
 });
+
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 builder.Services.AddSignalR();
 
